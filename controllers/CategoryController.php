6f82a953a737793b5534cbdfc649979bd03ae1e6
Andrey Kachanov <andrey170749@yandex.ru>
2018-01-10 12:50:06 +0300

    Add title, meta tags & category name viewed category

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -18,6 +18,7 @@ class CategoryController extends AppController
     public function actionIndex()
     {
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        $this->setMeta('Site_name');
         return $this->render('index', compact('hits'));
     }
 
@@ -25,6 +26,8 @@ class CategoryController extends AppController
     {
         $id = Yii::$app->request->get('id');
         $products = Product::find()->where(['category_id' => $id])->all();
-        return $this->render('view', compact('products'));
+        $category = Category::findOne($id);
+        $this->setMeta('Shop | ' . $category->name, $category->keywords, $category->description);
+        return $this->render('view', compact('products', 'category'));
     }
 }
