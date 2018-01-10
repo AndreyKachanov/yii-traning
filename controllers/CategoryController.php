430831498fea86d07a7ce3eb846b457dbb200280
Andrey Kachanov <andrey170749@yandex.ru>
2018-01-10 16:46:10 +0300

    Add pagination for the product category

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -11,6 +11,7 @@ namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
 use Yii;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -25,9 +26,12 @@ class CategoryController extends AppController
     public function actionView($id)
     {
         $id = Yii::$app->request->get('id');
-        $products = Product::find()->where(['category_id' => $id])->all();
+//        $products = Product::find()->where(['category_id' => $id])->all();
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination(['totalCount' =>$query->count(), 'pageSize' => 3, 'forcePageParam' => false, 'pageSizeParam' => false ] );
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $category = Category::findOne($id);
         $this->setMeta('Shop | ' . $category->name, $category->keywords, $category->description);
-        return $this->render('view', compact('products', 'category'));
+        return $this->render('view', compact('products', 'pages', 'category'));
     }
 }
