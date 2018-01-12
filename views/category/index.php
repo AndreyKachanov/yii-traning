ca613f20f6b4a64178268216aaad51d8d26e241c
Andrey Kachanov <andrey170749@yandex.ru>
2018-01-12 15:59:31 +0300

    Add card product

--- views/category/index.php
+++ views/category/index.php
@@ -123,16 +123,9 @@ use yii\helpers\Html;
                                     <div class="productinfo text-center">
                                         <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name]) ?>
                                         <h2>$<?= $hit->price?></h2>
-                                        <p><?= $hit->name?></p>
+                                        <p><a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $hit->id])?>"><?= $hit->name?></a></p>
                                         <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                     </div>
-<!--                                    <div class="product-overlay">-->
-<!--                                        <div class="overlay-content">-->
-<!--                                            <h2>$--><?//= $hit->price ?><!--</h2>-->
-<!--                                            <p>--><?//= $hit->name ?><!--</p>-->
-<!--                                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
                                         <?php if ($hit->new): ?>
                                             <?= Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'new']) ?>
                                         <?php endif; ?>
@@ -140,7 +133,6 @@ use yii\helpers\Html;
                                         <?php if ($hit->sale): ?>
                                             <?= Html::img("@web/images/home/sale.png", ['alt' => 'Распродажа', 'class' => 'new']) ?>
                                         <?php endif; ?>
-
                                 </div>
                                 <div class="choose">
                                     <ul class="nav nav-pills nav-justified">
