6f0e229978bfba47c24b0ab32e2b431881b379b5
Andrey Kachanov <andrey170749@yandex.ru>
2018-01-15 21:38:23 +0300

    Add authorization

---
+++ modules/admin/controllers/AppAdminController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 15.01.18
+ * Time: 19:03
+ */
+
+namespace app\modules\admin\controllers;
+
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+
+class AppAdminController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ]
+        ];
+    }
+
+}
--- views/category/index.php
+++ views/category/index.php
@@ -111,6 +111,7 @@ use yii\helpers\Html;
                 </div>
             </div>
 
+
             <div class="col-sm-9 padding-right">
 
                 <?php if (!empty($hits)): ?>
